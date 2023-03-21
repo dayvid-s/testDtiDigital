49296d0a9264b11e8cc9d818c99b1c1e2a093205
Dayvid <dayvid7santos2002@outlook.com>
2023-03-21 18:25:27 -0300

    feat: added methods at api

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,28 @@ server.use(express.json())
 
 const reminderNotes= []
 
+server.get('/reminderNotes', (req, res) => {
+    
+    return res.json(reminderNotes)
+})
+
+
+server.post('/reminderNotes', (req, res) => {
+    const {name} = req.body
+    const {userDate} = req.body
+    let userReminderNotes ={name, userDate}
+    reminderNotes.push(userReminderNotes)
+
+})
+
+
+server.delete('/reminderNotes/:index', (req, res) => {
+    const {index} = req.params
+
+    reminderNotes.splice(index, 1)
+    return res.json({ message: "Lembrete deletado com suceso"})
+
+})
 
 
 server.listen(5000);
