fd527b43587e3aac49f473dfd10cb90a459cbf21
Dayvid <dayvid7santos2002@outlook.com>
2023-03-21 07:52:15 -0300

    added styled-components

---
+++ src/theme/dark.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components'
+
+export const darkTheme : DefaultTheme = {
+  backGround: '#000000',
+  onBackGround: '#FFF',
+  secondary: '#171717',
+  gray300: '#434344',
+  rose: '#6c1e85',
+  roseBlue: '#7159c1',
+  opacity: 0.6
+}
---
+++ src/theme/light.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components'
+
+export const lightTheme : DefaultTheme = {
+  backGround: '#fcfcfc', 
+  onBackGround:'#000' ,
+  secondary: '#f0f0f0',
+  gray300: '#434344',
+  rose:'#6c1e85',
+  roseBlue:'#7159c1',
+  opacity: 0.6   
+
+}
